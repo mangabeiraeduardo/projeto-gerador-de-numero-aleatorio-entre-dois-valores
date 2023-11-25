3bc471c2cef3b5907621684d3399e2b7be011b87
Eduardo Mangabeira <eduardomangabeiraneto@gmail.com>
2023-11-24 21:04:34 -0300

    Projeto gerdor de número aleatório

---
+++ script.js
@@ -0,0 +1,7 @@
+function generateNumber() {
+  const min = Math.ceil(document.querySelector(".input-min").value);
+  const max = Math.floor(document.querySelector(".input-max").value);
+  const result = Math.floor(Math.random() * (max - min) + min);
+
+  alert(result);
+}
